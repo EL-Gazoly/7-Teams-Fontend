2591fb17af634c02e2de97bad64cf8dbe8d129dc
El-Gazoly <elgazoly11@gmail.com>
2023-12-11 11:25:22 +0200

    add:studnets keys

--- src/Components/StudentsView/Grid/index.tsx
+++ src/Components/StudentsView/Grid/index.tsx
@@ -5,7 +5,7 @@ const StudentsGridView = () => {
     <div className=" w-full grid grid-cols-4 gap-x-2 gap-y-[53px]">
       {
         new Array(8).fill(undefined).map((_,index) => (
-          <StudentCard />
+          <StudentCard key={index} />
         ))
       }
 
