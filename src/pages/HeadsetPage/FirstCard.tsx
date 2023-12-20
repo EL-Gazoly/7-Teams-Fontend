66e25fc5a41b43b8a8247443aeb8c4611998ba2a
EL-Gazoly <elgazoly11@gmail.com>
2023-12-20 20:38:26 +0200

    add error handling to headset page

--- src/pages/HeadsetPage/FirstCard.tsx
+++ src/pages/HeadsetPage/FirstCard.tsx
@@ -15,9 +15,13 @@ export function FirstCard({ device, deviceState }) {
   return (
     <div className='w-[565px] h-[375px] rounded-[21px] bg-light-bg py-5 px-12 relative flex flex-col items-end'
          style={{ boxShadow: '0px 3.588px 13.454px 0px rgba(91, 89, 89, 0.25)' }}>
-      <div className='absolute top-0 left-[8%] w-[116px] h-9 py-2 px-[6px] flex items-center justify-center gap-x-[6px] rounded-b-[3px] bg-primary'>
-        <div className='w-2 h-3 bg-white rounded-full' />
-        <span className='text-white'>متصل الان</span>
+      <div className={`absolute top-0 left-[8%] w-[116px] h-9 py-2 px-[6px] flex items-center justify-center gap-x-[6px] rounded-b-[3px]
+        ${device.student.length !==0 ? "bg-[#2DEC4C]" : "bg-disabled"}
+      bg-primary`}>
+        <div className='w-2 h-2 bg-white rounded-full mt-[2px]' />
+        <span className='text-white'>
+          {device.student.length !==0 ? "متصل" : "غير متصل"}
+           </span>
       </div>
 
       <Image src={OnlineIcon} width={43} height={43} />
@@ -26,7 +30,7 @@ export function FirstCard({ device, deviceState }) {
         <Image src={HeadsetImage} width={170} height={104} />
         <div className="flex flex-col gap-y-1 items-center text-text-black text-center">
           <span className='text-[22px] font-bold'>{device.student.length !== 0 ? device.student.name : "لا يوجد"}</span>
-          <span className='text-xs'>{device.name}</span>
+          <span className='text-xs font-poppins'>{device.name}</span>
         </div>
         <div className='flex flex-col mt-7 items-center gap-y-[14px]'>
           <div className="flex items-center gap-x-1">
--- src/pages/HeadsetPage/index.tsx
+++ src/pages/HeadsetPage/index.tsx
@@ -4,15 +4,17 @@ import ControlCard from '../../Components/ContraolCard'
 import SecondCard from './SecondCard';
 import ThridCard from './ThridCard';
 import FourthCard from './FourthCard';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { GetDevice } from '../../graphql/devices';
 import { useQuery } from '@apollo/client';
 import Loading from '../../Components/Loading';
 import db from '../../config/firebase'
 import { ref, update, onValue } from 'firebase/database'
+import { toast } from 'sonner';
 
 const HeadsetPage = () => {
   const { mac } = useParams<{ mac: string }>()
+  const navigate = useNavigate()
   const [isLoading, setIsLoading] = useState(true)
   const [deviceState, setDeviceState] = useState({});
   const ipcRenderer = (window as any).ipcRenderer;
@@ -32,28 +34,46 @@ const HeadsetPage = () => {
 
     const handleIPQuery = (snapshot) => {
       const ipInfo = snapshot.val();
-      if (ipInfo !== 'get') console.log(ipInfo);
-      ipcRenderer.send('connect', ipInfo);
+      if (ipInfo !== 'get') { 
+        ipcRenderer.send('connect', ipInfo);
+    }
     };
 
     onValue(deviceQuery, handleDeviceQuery);
     onValue(ipQuery, handleIPQuery);
 
-    const connectReplyHandler = (arg) => { if (arg === 'connected') console.log('connected'); };
+    const connectReplyHandler = (event, arg) => {
+      if (arg === "Connected") setIsLoading(false);
+    };
     ipcRenderer.on('connect-reply', connectReplyHandler);
 
-  }, [mac]);
+    const timeout = setTimeout(() => {
+      if (isLoading) {
+        navigate(-1)
+        toast.error(' اذا استمرت المشكله جرب توصيل الجهاز بالكمبيوتر مباشره ')
+        toast.error(' فشل الاتصال تاكد ان الجهاز متصل بنفس الشبكه ') 
+              }
+    }, 60000);
 
-  if (loading) return <Loading />;
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [mac, isLoading]);
+
   if (error) console.log(error.message);
   if (!device) return <p>Not found</p>;
-  if (device) console.log(device.deviceByMac);
 
   return (
     <div>
       <ControlCard icon="Headset" title=' نظارة الواقع الافتراضي ' neasted={true} />
-
-      <div className='flex flex-col mt-6 items-center gap-y-6 pb-5'>
+      {
+        loading || isLoading ?
+          <div className=' mt-[35%]'>
+            <Loading />
+          </div>
+        
+        : (
+          <div className='flex flex-col mt-6 items-center gap-y-6 pb-5'>
         <div className='w-full flex flex-row-reverse items-center gap-x-4'>
           {device && <FirstCard device={device.deviceByMac} deviceState={deviceState} />}
           <SecondCard ipcRenderer={ipcRenderer} />
@@ -63,6 +83,9 @@ const HeadsetPage = () => {
           <FourthCard />
         </div>
       </div>
+        )
+      }
+      
     </div>
   );
 }
