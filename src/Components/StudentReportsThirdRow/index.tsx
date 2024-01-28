2676d17f0baa4dc0f703a64747636f2b0567cd95
El-Gazoly <elgazoly11@gmail.com>
2024-01-28 18:07:33 +0200

    finish student reports

--- src/Components/StudentReportsThirdRow/index.tsx
+++ src/Components/StudentReportsThirdRow/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import StudentResults from './StudentResults'
+import ExpermientEnteranceCounter from './ExpermientEnteranceCounter'
 
 const StudentReportsThirdRow = () => {
   return (
     <div className='flex items-center gap-x-2'>
         <StudentResults />
+        <ExpermientEnteranceCounter />
       
     </div>
   )
