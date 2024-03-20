f4ed1b04bf2690a2e43f4f841a7aca2a4a6d950d
El-Gazoly <elgazoly11@gmail.com>
2024-03-20 15:11:38 +0200

    add more create students fields

---
+++ src/pages/CreateStudent/ChooseTeam.jsx
@@ -0,0 +1,160 @@
+import { FC, useEffect, useState } from 'react';
+import Select from 'react-select';
+import GroupIcon from '../../assets/Reports/group-dark.png'
+import { useThemeStore } from '../../stores/ThemeStore'
+const options = [
+  { value: 'High', label: 'الصف الثانوي', image: GroupIcon },
+  { value: 'Middle', label: 'الصف الاعدادي', image: GroupIcon },
+  { value: 'Primary', label: 'الصف الابتدائي', image: GroupIcon },
+]
+
+
+const ChooseGrade = (props) => {
+  const {dark} = useThemeStore();
+    const [selectedOption, setSelectedOption] = useState(props.selectedCourse);
+    
+    const handleChange = (selectedOption) => {
+      setSelectedOption(selectedOption);
+      props.setSelectedTeam(selectedOption.value)
+      
+    };
+    useEffect(() => {
+      if(props.selectedTeam === ''){
+        setSelectedOption(null);
+      }
+    }, [props.selectedTeam]);
+  
+    const customStyles = {
+      control: (provided) => ({
+        ...provided,
+        direction : 'rtl',
+        backgroundColor:  selectedOption? '#50D766' : dark ? '#1F242DAB' : '#F0F2F4',
+        backdropFilter: 'blur(73px)',
+        borderRadius: '4px',
+        cursor: 'pointer',
+        width: '178px',
+        border: 'none',
+        height: '66px',
+        boxShadow: 'none',
+        '&:after': {  // Add this pseudo-element for the icon
+            content: `url("data:image/svg+xml;charset=UTF-8,<svg xmlns='http://www.w3.org/2000/svg' width='22' height='23' viewBox='0 0 22 23' fill='none'><path d='M7.21607 2.69531L14.6971 2.69531C17.9467 2.69531 19.8839 4.63254 19.8839 7.88208V15.3542C19.8839 18.6127 17.9467 20.5499 14.6971 20.5499H7.22499C3.97545 20.5499 2.03823 18.6127 2.03823 15.3632V7.88208C2.0293 4.63254 3.96653 2.69531 7.21607 2.69531Z' fill='%23122333'/><path d='M10.9564 14.2206C11.126 14.2206 11.2956 14.1581 11.4295 14.0242L14.5809 10.8729C14.8397 10.614 14.8397 10.1855 14.5809 9.92659C14.322 9.6677 13.8935 9.6677 13.6346 9.92659L10.9564 12.6048L8.27818 9.92659C8.01929 9.6677 7.59078 9.6677 7.33188 9.92659C7.07299 10.1855 7.07299 10.614 7.33188 10.8729L10.4832 14.0242C10.6171 14.1581 10.7868 14.2206 10.9564 14.2206Z' fill='white'/></svg>")`,
+            position: 'absolute',
+          top: '60%',
+          left: '5%',
+          transform: 'translateY(-50%)',
+          marginRight: '10px',
+        '&:focus' :{
+          outline: 'none',
+        },
+        '&:hover': {
+          outline: 'none',
+          borderColor: '#292D32'
+        }
+        },
+        fontWeight: '500',
+        fontSize : '16px',
+       
+    
+      }),
+      singleValue: (provided) => ({
+        ...provided,
+        width: '100%',
+        color:  dark? 'white' : '#122333',
+        cursor: 'pointer',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: '10px',
+        fontSize: '16px',
+        marginLeft: '16px',
+      }),
+      option: (provided) => ({
+        ...provided,
+        width: '178px',
+        height: '81px',
+        backgroundColor:  dark ? '#1F242DAB' : '#F0F2F4',
+        color: 'white' ,
+        display: 'flex',
+        alignItems: 'center',
+        padding: '10px', 
+        cursor: 'pointer',
+        gap: '10px',
+        borderBottom : '1px solid rgba(255, 255, 255, 0.37)',
+        paddingLeft : '10px',
+        paddingRight : '10px',
+        '&:active' : {
+          backgroundColor: '#185AEA',
+        }
+      }),
+      menu: (provided) => ({
+        ...provided,
+        width: '178px',
+        direction : 'rtl',
+        maxHeight: '320px',
+        backgroundColor:  dark ? '#1F242DAB' : '#F0F2F4',
+        cursor: 'pointer',
+        gap: '8px',
+        overflow: 'hidden', 
+        zIndex: '1000',
+      }),
+      menuList: (provided) => ({
+        ...provided,
+        padding: 0,
+        cursor: 'pointer',
+        width: '100%',
+        zIndex: '1000',
+        overflowX: 'hidden',
+      }),
+      indicatorSeparator: (provided) => ({
+        ...provided,
+        display: 'none', 
+      }),
+      dropdownIndicator: (provided) => ({
+        ...provided,
+        display: 'none', 
+      }),
+      placeholder: (provided) => ({
+        ...provided,
+        color : "#FFFFFF87",
+        fontSize: '15px',
+        marginLeft: '10px',
+        
+        
+      }),
+      input: (provided) => ({
+        ...provided,
+        caretColor: 'transparent',
+        color: '#122333',
+      }),
+    
+      
+    };
+  
+    useEffect(() => {
+      if(props.emptyCourse){
+        setSelectedOption(null);
+        props.setEmptyCourse(false);
+      }
+  
+    }, [props.emptyCourse]);
+  return (
+    <div className="App">
+    <Select
+      options={options}
+      styles={customStyles}
+      placeholder="اختر المرحله"
+      value={selectedOption}
+      onChange={handleChange}
+      isSearchable={true}
+      getOptionLabel={(option) => (
+        <div className='selected flex items-center gap-x-6 text-xs font-medium'>
+          <img src={GroupIcon} alt={option.label} style={customStyles.optionImage} className=' w-10 h-10'/>
+          <span className={`${ selectedOption && selectedOption.value === "Ultrasound" ? 'text-[10px]' : ''}`}> {option.label} </span>
+        </div>
+      )}
+    />
+  </div>
+  )
+}
+
+export default ChooseGrade
--- src/pages/CreateStudent/index.tsx
+++ src/pages/CreateStudent/index.tsx
@@ -10,9 +10,26 @@ import { UploadImage } from './UploadImage';
 import { getStudents } from '../../graphql/students';
 import { GetStudents } from '../../graphql/reports';
 import AddWithExcel from '../../Components/AddWithExcelModal';
+import ChooseSchool from './ChooseSchool';
+import ChooseTeam from './ChooseTeam';
+import ChooseClass from './ChooseClass';
+import ChooseClassAlpha from './ChooseClassAlpha';
+import EyeSlashIcon from '../../assets/login/EyeSlashIcon.svg'
+import EyeIcon from '../../assets/login/EyeIcon.svg'
+import EyeDarkIcon from '../../assets/login/dark-eye.svg'
+import EyeSlashDarkIcno from '../../assets/login/dark-eye-slash.svg'
+import { useThemeStore } from '../../stores/ThemeStore';
 const CreateStudent = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedSchool, setSelectedSchool] = useState('');
+  const [selectedTeam, setSelectedTeam] = useState('');
+  const [selectedClass, setSelectedClass] = useState('');
+  const [selectedClassAlpha, setSelectedClassAlpha] = useState('');
+  const [isIconshowed, setIsIconshowed] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { dark } = useThemeStore();
 
   const [createStudentMutation, { data, loading, error }] = useMutation(createStudent,{
     refetchQueries: [{ query: getStudents }, { query: GetStudents}],
@@ -20,6 +37,8 @@ const CreateStudent = () => {
 
   const nameRef = useRef<HTMLInputElement>(null);
   const idRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordRef = useRef<HTMLInputElement>(null);
 
   const handleCreateStudent = async () => {
     const name = nameRef.current?.value;
@@ -46,10 +65,10 @@ const CreateStudent = () => {
   }
 
   return (
-    <>
+    <div className=' pb-8'>
       <ControlCard icon="Students" title=' الطلاب ' neasted={true}/>
       <div
-        className='mt-6 w-full h-[628px] bg-[#FDFDFE] dark:bg-primary-dark rounded-lg pt-[75px] px-[113px] flex flex-col gap-y-12'
+        className='mt-6 w-full h-[703px] bg-[#FDFDFE] dark:bg-primary-dark rounded-lg pt-[70px] px-[113px] flex flex-col gap-y-12'
         style={{
           boxShadow: '0px 3.812px 99.108px 0px rgba(0, 0, 0, 0.08)',
           backdropFilter: 'blur(24.30045509338379px)',
@@ -60,32 +79,81 @@ const CreateStudent = () => {
           setSelectedImage={setSelectedImage}
           setSelectedFile={setSelectedFile}
         />
-
-        <div className='flex flex-row-reverse items-center gap-x-[22px]'>
-          <div className='flex flex-col gap-y-[5px] text-right text-text-black dark:text-white'>
-            <label htmlFor='name' className='mr-1'>
-              الاسم
-            </label>
+        <div className=' flex flex-col items-center gap-y-4'>
+          <div className='flex flex-row-reverse items-center gap-x-[22px]'>
+      
             <input
               type='text'
               className='text-right w-[380px] rounded-lg h-[66px] bg-[#F0F2F4] dark:bg-dark-item/70 px-4'
               placeholder='اسم الطالب هنا'
               ref={nameRef}
             />
-          </div>
-          <div className='flex flex-col gap-y-[5px] text-right text-text-black dark:text-white'>
-            <label htmlFor='id' className='mr-1'>
-              رقم الطالب
-            </label>
+        
             <input
               type='text'
               className='text-right w-[380px] rounded-lg h-[66px] bg-[#F0F2F4] dark:bg-dark-item/70 px-4'
               placeholder='رقم الطالب هنا'
               ref={idRef}
             />
+        
           </div>
+
+          <div className=' flex flex-row-reverse items-center gap-x-7'>
+            <ChooseSchool
+              selectedSchool={selectedSchool}
+              setSelectedSchool={setSelectedSchool}
+            />
+            <div className='flex items-center gap-x-3'>
+              <ChooseTeam 
+                selectedTeam={selectedTeam}
+                setSelectedTeam={setSelectedTeam}
+              />
+              <ChooseClass 
+                selectedClass={selectedClass}
+                setSelectedClass={setSelectedClass}
+              />
+            </div>
+
+          </div>
+
+          <div className=' flex flex-row-reverse justify-start w-full'>
+              <ChooseClassAlpha 
+                selectedClassAlpha={selectedClassAlpha}
+                setSelectedClassAlpha={setSelectedClassAlpha}
+              />
+          </div>
+
+          <div className=' flex flex-row-reverse items-center gap-x-7'>
+            <div className='w-[380px] rounded-lg h-[66px] bg-[#F0F2F4] dark:bg-dark-item/70 px-4 flex items-center '>
+                    {dark? 
+                    <img src={showPassword ? EyeDarkIcon :  EyeSlashDarkIcno} alt="" onClick={()=> setShowPassword(!showPassword)}   className={` cursor-pointer w-6 h-6 ${isIconshowed? 'block' : 'hidden'}`} />
+                    :
+                    <img src={showPassword ? EyeIcon :  EyeSlashIcon} alt="" onClick={()=> setShowPassword(!showPassword)}    className={` cursor-pointer ${isIconshowed? 'block' : 'hidden'}`}/>
+                    }
+
+                    <input type={showPassword? "text" : "password"} className=' text-right flex-1  h-full bg-transparent
+                    ' placeholder=' الرقم السري هنا' ref={passwordRef} 
+                        onChange={(e) => setIsIconshowed(e.target.value)}
+                    />
+            </div>
+            <div className='w-[380px] rounded-lg h-[66px] bg-[#F0F2F4] dark:bg-dark-item/70 px-4 flex items-center '>
+                    {dark? 
+                    <img src={showPassword ? EyeDarkIcon :  EyeSlashDarkIcno} alt="" onClick={()=> setShowPassword(!showPassword)}   className={` cursor-pointer w-6 h-6 ${isIconshowed? 'block' : 'hidden'}`} />
+                    :
+                    <img src={showPassword ? EyeIcon :  EyeSlashIcon} alt="" onClick={()=> setShowPassword(!showPassword)}    className={` cursor-pointer ${isIconshowed? 'block' : 'hidden'}`}/>
+                    }
+
+                    <input type={showPassword? "text" : "password"} className=' text-right flex-1  h-full bg-transparent
+                    ' placeholder=' تاكيد الرقم السري هنا' ref={confirmPasswordRef} 
+                        onChange={(e) => setIsIconshowed(e.target.value)}
+                    />
+            </div>
+
+          </div>
+
         </div>
-        <div className='w-full items-center justify-center flex flex-row-reverse gap-x-7 mt-20'>
+
+        <div className='w-full items-center justify-center flex flex-row-reverse gap-x-7 mt-2'>
           <Button
             className='w-[177px] h-[51px] py-2 px-4 rounded-lg flex items-center justify-center gap-x-2 bg-[#4E5464]'
             onPress={handleCreateStudent}
@@ -97,7 +165,7 @@ const CreateStudent = () => {
           <AddWithExcel />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
