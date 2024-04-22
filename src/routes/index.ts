8d7cd6d202eadb502241e0bd8226100e66c7c95b
El-Gazoly <elgazoly11@gmail.com>
2024-04-22 13:12:49 +0200

    refactor routes

---
+++ src/routes/index.ts
@@ -0,0 +1,63 @@
+import HeadsetsPage from "../pages/Headset";
+import CoursesPage from "../pages/Courses";
+import CoursePage from "../pages/CoursePage";
+import StudentsPage from "../pages/Students";
+import CreateStudent from "../pages/CreateStudent";
+import Library from "../pages/LibraryPage";
+import CertificatesPage from "../pages/Certificates";
+import GeneralSettingsPage from "../pages/Settings";
+import AdminsPage from "../pages/Admins";
+import CreateAdmin from "../pages/CreateAdmin";
+import RolesPage from "../pages/Roles";
+import HeadsetPage from "../pages/HeadsetPage";
+import UpdateAdmin from "../pages/UpdateAdmin";
+import CreateRolePage from "../pages/CreateRole";
+import UpdateRolePage from "../pages/UpdateRole";
+import Dashboard from "../pages/Dashboard";
+import ReportsPage from "../pages/ReportsPage";
+import StudentsReports from "../pages/StudentsReports";
+import StudentReportsPage from "../pages/StudentReportPage";
+import StagesReportsPage from "../pages/StagesReportsPage";
+import StageReportPage from "../pages/StageReportPage";
+import ClassesReportsPage from "../pages/ClassesReportsPage";
+import ClassReportPage from "../pages/ClassReportPage";
+import LogsPage from "../pages/LogsPage";
+import SchoolsPage from "../pages/SchoolsPage";
+import CreateSchool from "../pages/CreateSchool";
+import UpdateSchool from "../pages/UpdateSchool";
+import SchoolReports from "../pages/SchoolReports";
+import SchoolReport from "../pages/SchoolReport";
+
+const routes = [
+  { path: "/headsets", component: HeadsetsPage },
+  { path: "/headsets/:mac", component: HeadsetPage },
+  { path: "/courses", component: CoursesPage },
+  { path: "/students", component: StudentsPage },
+  { path: "/courses/:course", component: CoursePage },
+  { path: "/students/create", component: CreateStudent },
+  { path: "/library", component: Library },
+  { path: "/certificates", component: CertificatesPage },
+  { path: "/settings", component: GeneralSettingsPage },
+  { path: "/settings/admins", component: AdminsPage },
+  { path: "/settings/admins/create", component: CreateAdmin },
+  { path: "/settings/admins/update/:id", component: UpdateAdmin },
+  { path: "/settings/roles", component: RolesPage },
+  { path: "/settings/roles/create", component: CreateRolePage },
+  { path: "/settings/roles/update/:id", component: UpdateRolePage },
+  { path: "/dashboard", component: Dashboard },
+  { path: "/reports", component: ReportsPage },
+  { path: "/reports/students", component: StudentsReports },
+  { path: "/reports/students/:id", component: StudentReportsPage },
+  { path: "/reports/stages", component: StagesReportsPage },
+  { path: "/reports/stages/:stage", component: StageReportPage },
+  { path: "/reports/classes", component: ClassesReportsPage },
+  { path: "/reports/classes/:stage/:class", component: ClassReportPage },
+  { path: "/logs", component: LogsPage },
+  { path: "/schools", component: SchoolsPage },
+  { path: "/schools/create", component: CreateSchool },
+  { path: "/schools/update/:id", component: UpdateSchool },
+  { path: "/reports/schools", component: SchoolReports },
+  { path: "/reports/schools/:id", component: SchoolReport }
+];
+
+export default routes;
